a452df7df47c5c85316cc6762c6286a0f093a8be
Eder <edermartioli@gmail.com>
2026-01-12 13:40:12 -0300

    Change output directory for version.py in setup

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ except Exception:
     version = '{version}'
 """.lstrip()
 
-setup(use_scm_version={'write_to': os.path.join('spectrum', 'version.py'),
+setup(use_scm_version={'write_to': os.path.join('espectro', 'version.py'),
                        'write_to_template': VERSION_TEMPLATE})
